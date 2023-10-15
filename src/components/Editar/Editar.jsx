070aa85a411c1a0e0d9499bfc5086656c1afba71
Charlie-Pencal <cpencal@gmail.com>
2023-10-15 16:16:56 -0300

    feat: Criado estilização para barra de edição

--- src/components/Editar/Editar.jsx
+++ src/components/Editar/Editar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {editarBarra} from '../Editar/Editar.module.css'
 
 const Editar = (props) => {
     return (
         <div>
             <input
+                className={editarBarra}
                 type="text"
                 value={props.nomeValue}
                 onChange={props.editarNome}
