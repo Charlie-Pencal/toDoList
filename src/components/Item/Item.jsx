542097559bacd4eb08827d54d70206c733fa5858
Charlie-Pencal <cpencal@gmail.com>
2023-10-15 16:17:48 -0300

    fix: Alteração para os icones ficarem a direita

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,12 +5,16 @@ import itemStyle from './Item.module.css'
 const Item = (props) => {
   return (
     <div className={itemStyle.listaTarefas}>
-        <Check status={props.tarefa.status} onCheck={props.onCheck}/>
-        {props.tarefa.nome}
-        <button onClick={props.onEdit}><img src="/pencil.svg" alt="" /></button>
-        <button onClick={props.onRemove}>
-        <img src="/trash.svg" alt="" />
+      <Check status={props.tarefa.status} onCheck={props.onCheck} />
+      {props.tarefa.nome}
+      <div>
+        <button onClick={props.onEdit}>
+          <img src="/pencil.svg" alt="" />
         </button>
+        <button onClick={props.onRemove}>
+          <img src="/trash.svg" alt="" />
+        </button>
+      </div>
     </div>
   )
 }
