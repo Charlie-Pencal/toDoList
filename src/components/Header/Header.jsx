dddf31e5ac6739631e11842adad62bf18f980507
Charlie-Pencal <cpencal@gmail.com>
2023-10-15 16:18:34 -0300

    fix: retirado input do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Header.module.css'
 
+
+
 const Header = () => {
   return (
     <div className={styles.header}>
         <img src="/toDoList.svg" alt="" />
         <div className={styles.div}>
-            <input type="text" className={styles.buscar} placeholder='Buscar'/>
             <li className={styles.navegacaoFiltros}>
                 <a className={styles.filtros} href="#pendente">Pendente</a>
                 <a className={styles.filtros} href="#concluido">Concluido</a>
@@ -18,4 +19,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
